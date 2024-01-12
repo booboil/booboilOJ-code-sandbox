028d7295ae38c647cfaaaebbd7bf5864b7a18a34
张新荣 <2914424377@qq.com>
2024-01-12 16:50:31 +0800

    编译代码，得到class文件

--- src/main/java/com/booboil/codesandbox/JavaNativeCodeSandbox.java
+++ src/main/java/com/booboil/codesandbox/JavaNativeCodeSandbox.java
@@ -4,11 +4,13 @@ import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.resource.ResourceUtil;
 import com.booboil.codesandbox.model.ExecuteCodeRequest;
 import com.booboil.codesandbox.model.ExecuteCodeResponse;
-import org.apache.logging.log4j.util.ProcessIdUtil;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
@@ -16,7 +18,7 @@ import java.util.UUID;
 /**
  * Java 原生代码沙箱实现（直接复用模板方法）
  */
-public class JavaNativeCodeSandbox implements CodeSandbox{
+public class JavaNativeCodeSandbox implements CodeSandbox {
 
     private static final String GLOBAL_CODE_DIR_NAME = "tmpCode";
 
@@ -41,7 +43,7 @@ public class JavaNativeCodeSandbox implements CodeSandbox{
         String code = executeCodeRequest.getCode();
         String language = executeCodeRequest.getLanguage();
 
-        // 1.把用户的代码保存为文件
+        // 1. 把用户的代码保存为文件
         String userDir = System.getProperty("user.dir");
         String globalCodePathName = userDir + File.separator + GLOBAL_CODE_DIR_NAME;
         // 判断全局代码目录是否存在，没有则新建
@@ -54,7 +56,49 @@ public class JavaNativeCodeSandbox implements CodeSandbox{
         String userCodePath = userCodeParentPath + File.separator + GLOBAL_JAVA_CALSS_NAME;
         // 写入文件
         File userCodeFile = FileUtil.writeString(code, userCodePath, StandardCharsets.UTF_8);
+
+        // 2. 编译代码，得到class文件
+        String compileCmd = String.format("javac -encoding utf-8 %s", userCodeFile.getAbsolutePath());
+        try {
+            Process compileProcess = Runtime.getRuntime().exec(compileCmd);
+            // 等待程序执行，获取错误码
+            int exitValue = compileProcess.waitFor();
+            // 正常退出
+            if (exitValue == 0) {
+                System.out.println("编译成功");
+                // 分批获取进程的正常输出
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(compileProcess.getInputStream()));
+                StringBuilder compileOutputStringBuilder = new StringBuilder();
+                // 逐行执行
+                String compileOutputLine;
+                while ((compileOutputLine = bufferedReader.readLine()) != null) {
+                    System.out.println(compileOutputLine);
+                }
+                System.out.println(compileOutputStringBuilder);
+            } else {
+                // 异常输出
+                System.out.println("编译失败，错误码：" + exitValue);
+                // 分批获取进程的正常输出
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(compileProcess.getInputStream()));
+                StringBuilder compileOutputStringBuilder = new StringBuilder();
+                // 逐行执行
+                String compileOutputLine;
+                while ((compileOutputLine = bufferedReader.readLine()) != null) {
+                    System.out.println(compileOutputLine);
+                }
+                // 分批获取进程的错误输出
+                BufferedReader errorBufferedReader = new BufferedReader(new InputStreamReader(compileProcess.getInputStream()));
+                StringBuilder errorCompileOutputStringBuilder = new StringBuilder();
+
+                // 逐行执行
+                String errorCompileOutputLine;
+                while ((errorCompileOutputLine = errorBufferedReader.readLine()) != null) {
+                    errorCompileOutputStringBuilder.append(errorCompileOutputLine);
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         return null;
     }
-
 }
