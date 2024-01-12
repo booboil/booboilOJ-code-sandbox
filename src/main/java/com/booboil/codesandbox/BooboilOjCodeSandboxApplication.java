30ea5fc54991b32d0922819efa5eb4e381b7ec93
张新荣 <2914424377@qq.com>
2024-01-12 10:28:21 +0800

    项目初始化

---
+++ src/main/java/com/booboil/codesandbox/BooboilOjCodeSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.booboil.codesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooboilOjCodeSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooboilOjCodeSandboxApplication.class, args);
+    }
+
+}
